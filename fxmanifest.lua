bc0423310c0ea77ddc1b15cf1e150dfbb46301b6
Lowsocket <150552361+Lowsocket@users.noreply.github.com>
2023-11-13 10:24:02 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Docvx & Lowsocket'
+name 'Quad Motor Tricks'
+description 'Tricks resource for FiveM servers'
+github 'https://github.com/Lowsocket'
+version '1.1'
+
+client_scripts {
+	'config.lua',
+	'main.lua'
+}
+
+fivem_checker 'yes'
